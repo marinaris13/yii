55f44650b107751a799ccff68de4192c2a495d1a
Marina Belikova <marina@belikova.ru>
2016-05-30 21:48:43 +0600

    change config db

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=warehouse',
     'username' => 'root',
-    'password' => '',
+    'password' => 'mysql',
     'charset' => 'utf8',
 ];
